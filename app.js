b4440431192a991bad73e421ee47a819946d3cd1
Daniel Martinez <dmartz86@gmail.com>
2015-03-04 09:38:30 -0500

    Refactor view. Add stand by request. Add responsive view.

--- app.js
+++ app.js
@@ -12,26 +12,31 @@ app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
-	res.render('index');
+  res.render('index');
 });
 
 app.get('/generate/:units', function(req, res) {
-	var uuidList = [];
-	var limit = 1000;
-	var units = parseInt(req.params.units);
-	var startTime = new Date().getTime();
+  var uuidList = [];
+  var limit = 1000;
+  var units = parseInt(req.params.units);
+  var startTime = new Date().getTime();
 
-	if ( units > 0 ){
-		if ( units >limit ){ units = limit;}
-		for ( var i = 0; i < units; i++ ) {
-			uuidList.push(uuid.v4());
-		}
-	}
+  if ( units > 0 ){
+    if ( units >limit ){ units = limit;}
+    for ( var i = 0; i < units; i++ ) {
+      uuidList.push(uuid.v4());
+    }
+  }
 
-	var endTime = new Date().getTime();
-	res.json({uuids: uuidList, time: (endTime - startTime)});
+  var endTime = new Date().getTime();
+  res.json(
+    {
+      uuids: uuidList,
+      time: (endTime - startTime)
+    }
+  );
 });
 
 http.createServer(app).listen(app.get('port'), function() {
-	console.log('Express server listening on port ' + app.get('port'));
+  console.log('Express server listening on port ' + app.get('port'));
 });
--- public/js/ng/controller.js
+++ public/js/ng/controller.js
@@ -4,21 +4,24 @@ window.app.controller('Generator', ['$scope', '$http', function($scope, $http) {
   $scope.units = 0;
   $scope.uuids = [];
   $scope.execTime = 0;
-
+  $scope.wait = false;
 
   $scope.generate = function () {
     if( parseInt( $scope.units ) > limit ) {
       $scope.units = limit;
     }
 
+    $scope.wait = true;
     $http.get( '/generate/' + $scope.units )
     .success( function( data ) {
       $scope.uuids = data.uuids;
       $scope.execTime = data.time;
+      $scope.wait = false;
     })
     .error( function ( err ) {
       console.log( 'error' );
       console.log( err );
+      $scope.wait = false;
     });
   };
 
